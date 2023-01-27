9aa0c86bf1b38bac124a9202aefb410552d486d2
LexaChikov <clarindclaudfish@gmail.com>
2023-01-27 06:43:01 -0500

    tested vk_api 01

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from random import randrange
+from config import user_token, group_token
 
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-token = input('Token: ')
+# token = input('Token: ')
 
-vk = vk_api.VkApi(token=token)
+vk = vk_api.VkApi(token=group_token)
 longpoll = VkLongPoll(vk)
 
 
 def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+    vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7), })
+
+
+def get_user_info(user_id):
+    user_info = vk.method('users.get', {'user_id': user_id})
+    # print(user_info)
+    name = user_info[0]['first_name']
+    return name
 
 
 for event in longpoll.listen():
@@ -18,10 +26,12 @@ for event in longpoll.listen():
 
         if event.to_me:
             request = event.text
+            name = get_user_info(event.user_id)
 
-            if request == "привет":
-                write_msg(event.user_id, f"Хай, {event.user_id}")
-            elif request == "пока":
-                write_msg(event.user_id, "Пока((")
+            if request == "hi":
+                write_msg(event.user_id, f"Хай, {name}")
+
+            elif request == "bye":
+                write_msg(event.user_id, f"Пока(( {name}")
             else:
-                write_msg(event.user_id, "Не поняла вашего ответа...")
+                write_msg(event.user_id, f"{name}. Bot Не понял вашего ответа...")
