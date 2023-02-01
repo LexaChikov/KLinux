2abb459e178b0dd6c1071cfa06cdc47680d439ae
LexaChikov <clarindclaudfish@gmail.com>
2023-02-01 06:06:53 -0500

    add file bot

---
+++ config.py
@@ -0,0 +1,13 @@
+user_token = "vk1.a.6iIDhcsfQgwoglf0iSo4YLQvZF4osn68Ra9r8BkdIHBb0J7O42k_zv62PAT8AwU5PS0_3DfVfU_6iJ_GGoKRbKPArMiE3ZfFedAee9U_2dScDbqJc2kj13uv6Ut2DD4I6TVK8cBtvDgp6TRj32BXjbzceyy_8zQzVKkNKBjAMlAdBggEf3WfvL27Mtip_7NYbSEJAJ8m7PFeqze3IOoHMw"
+  # токен пользователя
+
+group_token = "vk1.a.6bgDSJurd7RmZZlscGHPlunYmonTgHx7govU3stoes35lSOlBWgsQs_3MTOyTJLW_T-_MzlBBXhznn705omovEUqJVGdmJDroFaikwwleWBG4p3LOdCrTMoxfgFnNmctT6IrbBtRIgromvtGGHPf9Bc_JjC0VIzSPba9Pj9Y2mOivkGNcYMRqCdUZu4gtzEOUUB57Pt4zggbIzFVnv5rdw"
+  # токен сообщества
+
+offset = 0 #сдвиг
+line = range(0, 1000) # последовательность для перебора найденных пользователей
+
+host = "127.0.0.1"
+user = "postgres"
+password = "5a64Postgres5a64"
+db_name = "bot_users"
